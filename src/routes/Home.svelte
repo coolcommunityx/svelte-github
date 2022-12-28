e77370d84e26c72518177791e62b0221a3dd24a0
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-28 10:12:49 +0530

    starter code 😎

---
+++ src/routes/Home.svelte
@@ -0,0 +1,43 @@
+<script>
+import GithubIcon from "../components/GithubIcon.svelte";
+
+    let username;
+    const enterPressed = event => {
+        if(event.keyCode == 13) {
+            window.location.href += "#/users/"+username
+        }
+    }
+</script>  
+    <main>
+      
+          <div class="initial">
+            <div class="search">
+              <GithubIcon />
+              <h2 class="title pt-2">Search Github by Username</h2>
+              <input autofocus class="input" bind:value={username} type="text" on:keyup={(event) => enterPressed(event)} />
+            </div>
+          </div>
+            
+    </main>
+    
+    <style>
+      .initial {
+        background-color: #92122Daa;
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .initial .search {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        width: 300px;
+      }
+      .title {
+        color: whitesmoke;
+      }
+    </style>
